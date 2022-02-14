0496d547bf1fcfd8392a53029c01307b8cbcfdba
nitin-hugar <nhugar3@gatech.edu>
2022-02-13 22:40:38 -0500

    main file completed

--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -88,18 +88,12 @@ int main(int argc, char* argv[])
 
     // Destroy and clear memory: Check if this is needed !
 
-    if (ppfInputAudioData == 0 || ppfOutputAudioData == 0) // Also check if 'or' is needed
-    {
-        CAudioFileIf::destroy(phInputAudioFile);
-        CAudioFileIf::destroy(phOutputAudioFile);
-        return -1;
-    }
-    if (ppfInputAudioData[0] == 0 || ppfOutputAudioData[0] == 0)
-    {
-        CAudioFileIf::destroy(phInputAudioFile);
-        CAudioFileIf::destroy(phOutputAudioFile);
-        return -1;
-    }
+//    if (ppfInputAudioData[0] == 0 || ppfOutputAudioData[0] == 0)
+//    {
+//        CAudioFileIf::destroy(phInputAudioFile);
+//        CAudioFileIf::destroy(phOutputAudioFile);
+//        return -1;
+//    }
 
     //============================================================================
     // Create CombFilters and allocate memory
@@ -115,44 +109,41 @@ int main(int argc, char* argv[])
 
 
     //============================================================================
-    //
+    // Read, Filter and Write Audio
     //============================================================================
 
 
-
-    // get audio data and write it to the output text file (one column per channel)
-    while (!phAudioFile->isEof())
+    while (!phInputAudioFile->isEof())
     {
         // set block length variable
-        long long iNumFrames = kBlockSize;
+        long long int iNumFrames = kBlockSize;
 
         // read data (iNumOfFrames might be updated!)
-        phAudioFile->readData(ppfAudioData, iNumFrames);
+        phInputAudioFile->readData(ppfInputAudioData, iNumFrames);
+        pCombFilter->process(ppfInputAudioData, ppfOutputAudioData, iNumFrames);
+        phOutputAudioFile->writeData(ppfOutputAudioData, iNumFrames);
 
-        cout << "\r" << "reading and writing";
-
-        // write
-        for (int i = 0; i < iNumFrames; i++)
-        {
-            for (int c = 0; c < stFileSpec.iNumChannels; c++)
-            {
-                hOutputFile << ppfAudioData[c][i] << "\t";
-            }
-            hOutputFile << endl;
-        }
     }
 
-    cout << "\nreading/writing done in: \t" << (clock() - time) * 1.F / CLOCKS_PER_SEC << " seconds." << endl;
+    cout << "Filtering complete! The output audio is saved at : " << sOutputFilePath << endl;
 
-    //////////////////////////////////////////////////////////////////////////////
-    // clean-up (close files and free memory)
-    CAudioFileIf::destroy(phAudioFile);
-    hOutputFile.close();
+    //============================================================================
+    // Clean up and free memory
+    //============================================================================
 
-    for (int i = 0; i < stFileSpec.iNumChannels; i++)
-        delete[] ppfAudioData[i];
-    delete[] ppfAudioData;
-    ppfAudioData = 0;
+    phInputAudioFile -> closeFile();
+    phOutputAudioFile -> closeFile();
+    CAudioFileIf::destroy(phInputAudioFile);
+    CAudioFileIf::destroy(phOutputAudioFile);
+    for(int m = 0; m < stFileSpec.iNumChannels; m++ )
+    {
+        delete[] ppfInputAudioData;
+        delete[] ppfOutputAudioData;
+    }
+    delete[] ppfInputAudioData;
+    delete[] ppfOutputAudioData;
+    ppfInputAudioData = nullptr;
+    ppfOutputAudioData = nullptr;
 
     // all done
     return 0;
@@ -165,7 +156,5 @@ void     showClInfo()
     cout << "MUSI6106 Assignment Executable" << endl;
     cout << "(c) 2014-2022 by Alexander Lerch" << endl;
     cout  << endl;
-
-    return;
 }
 
